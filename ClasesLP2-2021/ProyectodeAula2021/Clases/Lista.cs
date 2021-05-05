a0fa6f67fb22aaa247b1dc2278d4ffe47c97a929
JuliaRecalde <63506698+JuliaRecalde@users.noreply.github.com>
2021-05-04 22:33:36 -0400

    listas ejercitario

--- ClasesLP2-2021/ProyectodeAula2021/Clases/Lista.cs
+++ ClasesLP2-2021/ProyectodeAula2021/Clases/Lista.cs
@@ -9,65 +9,124 @@ namespace ProyectodeAula2021.Clases
 {
     public class Lista
     {
-        String nombre ;
-        String apellido;
-        public int matricula {get; set;}
-        String carrera;
 
-        public String Carrera
+        /* 1) Escriba un programa que inserte en orden 25 enteros al azar, del 0 al 100, en una lista
+                ligada. El programa debe calcular la suma de los elementos y el promedio en punto flotante de ellos. */
+
+        public void Numero_Random()
         {
-            set  {this.carrera = value; }
-            get { return this.carrera; }
-        }
-
-       // recorre_interativo();
-       //  crea_inicio();
-
-        public void setAtributos(string nombre, string apellido)
-        {
-            this.nombre = nombre;
-            //this utilizamos para hacer referencia a el atributo nombre de esta clase Lista.
-            this.apellido = apellido;
-        }
-
-        public void MetodoVacio()
-        { }
-
-        public String getNombre()
-        {
-            return this.nombre;
-        }
-
-        public String getApellido()
-        {
-            return this.apellido;
-        }
-
-        //string perro { get; set; }
-
-
-        static void ImprimirLista()
-        {
-            string profe = "Andrea";
-            List<string> listaAlumnos = new List<string>();
-
-            listaAlumnos.Add("Augusto");
-            listaAlumnos.Add("Meli");
-            listaAlumnos.Add("Edgar");
-            listaAlumnos.Add(profe);
-           
-            Console.WriteLine(listaAlumnos[1]);
-            int contador = 0; 
-            foreach (string alumno in listaAlumnos)   // para cada elemento
-            //for (indiceInicio; incremento; condicionStop (cantidad de elementos))
-            //for (int i = 0; i < cantidad; i++ )
+            
+            List<int> enteros = new List<int>();
+            Random aleatorio = new Random();
+            int num;
+            int i;
+            int suma = 0;
+            for (i = 0; i < 25; i++)
             {
-                //Console.WriteLine(alumno);
-                Console.WriteLine("Elemento #{"+contador+"}--> "+alumno);
+                num = aleatorio.Next(0, 101);
+                enteros.Add(num);
+                suma = suma + num;
+            }
+            float promedio = suma / 25;
+            Console.WriteLine("\nLa suma de los elementos es: " + suma);
+            Console.WriteLine("\nEl promedio es: " + promedio);
+        }
+        
 
-                Console.WriteLine("Cantidad de elementos en la lista = " + listaAlumnos.Count().ToString());
+        /*Escriba un programa que genere una lista ligada de 10 caracteres, y que después genere una
+            copia de la lista en orden inverso. */
 
-                contador++;
+        public void Inverso()        //Ejercicio 2
+        {
+            List<string> caracter = new List<string>();
+            List<string> invierte = new List<string>();
+            caracter.Add("j");
+            caracter.Add("u");
+            caracter.Add("l");
+            caracter.Add("i");
+            caracter.Add("a");
+            caracter.Add("r");
+            caracter.Add("e");
+            caracter.Add("c");
+            caracter.Add("l");
+            caracter.Add("d");
+            caracter.Add("e");
+            int i;
+            for (i = 0; i < 11; i++)
+            {
+                invierte.Add(caracter[10 - i]);
+            }
+            for (i = 0; i < 11; i++)
+            {
+                Console.WriteLine("\n" + caracter[i]);
+            }
+            Console.WriteLine("Frase invertida:");
+            for (i = 0; i < 11; i++)
+            {
+                Console.WriteLine("\n" + invierte[i]);
+            }
+        }
+
+        public void Imprimelista()
+        {
+            int i;
+            int j = 9;
+            bool ban = false;
+            List<int> enteros = new List<int>();
+            for (i = 0; i < 10; i++)
+            {
+                enteros.Add(i + 1);
+            }
+            Console.WriteLine("Esta es la lista ordenada:");
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine(enteros[i]);
+            }
+            Console.WriteLine("\nEsta es la lista Invertida:");
+            imprimeListaInversa(j);
+            void imprimeListaInversa(int cont)
+            {
+                if (cont != -1)
+                {
+                    Console.WriteLine(enteros[cont]);
+                    cont--;
+                    imprimeListaInversa(cont);
+                }
+            }
+        }
+
+
+
+        /* (Impresión recursiva de una lista en orden inverso) Escriba una función imprimeListaInversa,
+                que recursivamente despliegue los elementos de una lista en orden inverso. Utilice su función
+                en un programa de prueba que genere una lista ordenada de enteros y que imprima la lista en
+                orden inverso. */
+
+        public void ImprimelistaInversa()
+        {
+            int i;
+            int a = 4;
+            
+            List<int> numeros = new List<int>();
+            for (i = 0; i < 5; i++)
+            {
+                numeros.Add(i + 1);
+            }
+            Console.WriteLine("Esta es la lista ordenada:");
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+            Console.WriteLine("\nLa lista invertida es:");
+            imprimir(a);
+            void imprimir(int cont)
+            {
+                if (cont != -1)
+                {
+                    Console.WriteLine(numeros[cont]);
+                    cont--;
+                    imprimir(cont);
+                }
             }
         }
     }
--- ClasesLP2-2021/ProyectodeAula2021/Program.cs
+++ ClasesLP2-2021/ProyectodeAula2021/Program.cs
@@ -19,57 +19,12 @@ namespace ProyectodeAula2021
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Login());
 
-            //Console.WriteLine("Hola Mundo");
-
-            // Call the constructor that has one parameter.
-            //var person2 = new Persona();
-
-            //Console.WriteLine(person2.Nombre);   
-            //Console.WriteLine(person2.Apellido);
-            //Console.WriteLine(person2);
-            
-
-            Lista objetoLista = new Lista();
-
-            objetoLista.setAtributos("Mauro", "Gimenez");
-
-            Console.WriteLine("El primer alumno de la clase es: "+objetoLista.getNombre());
-            Console.WriteLine("Los demas atributos del objeto Mauro son: " + objetoLista.getApellido());
-
-            objetoLista.Carrera = "Informatica"; //uso del set
-            Console.WriteLine("La carrera de Mauro es: "+objetoLista.Carrera); //uso del get
-
-            objetoLista.matricula = 4324; //uso del set
-            Console.WriteLine("La matricula de Mauro es: " + objetoLista.matricula); //uso del get
-
-            //ImprimirLista();
+            Lista lista = new Lista();
+            //lista.Numero_Random();
+            //lista.Inverso();
+            lista.ImprimelistaInversa();
         }
 
 
-        static void ImprimirLista()
-        {
-            string profe = "Andrea";
-            List<string> listaAlumnos = new List<string>();
-
-            listaAlumnos.Add("Augusto");
-            listaAlumnos.Add("Meli");
-            listaAlumnos.Add("Edgar");
-            listaAlumnos.Add(profe);
-
-            Console.WriteLine(listaAlumnos[1]);
-
-            int contador = 0;
-            foreach (string alumno in listaAlumnos)   // para cada elemento
-            //for (indiceInicio; incremento; condicionStop (cantidad de elementos))
-            //for (int i = 0; i < cantidad; i++ )
-            {
-                //Console.WriteLine(alumno);
-                Console.WriteLine("Elemento #{" + contador.ToString() + "}--> " + alumno);
-                contador++;
-            }
-
-            Console.WriteLine("Cantidad de elementos en la lista =" + listaAlumnos.Count().ToString());
-
-        }
     }
 }
